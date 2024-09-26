f3397d2a6c27364b0771d2858032aa7ab3513867
s2-UI <samueldf83@gmail.com>
2024-09-26 18:07:44 +0200

    Update dependencies and add Astro Vercel integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,14 +4,14 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
-import node from '@astrojs/node';
+// import node from '@astrojs/node';
+
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
   output: 'server',
 
-  adapter: node({
-    mode: 'standalone'
-  })
+  adapter: vercel()
 });
