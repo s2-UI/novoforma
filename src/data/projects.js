e71482ab532721b042065c5749f3868e2fd829b0
s2-UI <samueldf83@gmail.com>
2024-10-06 13:01:24 +0200

    Refactor GalleryImage component to use dynamic title and link

---
+++ src/data/projects.js
@@ -0,0 +1,100 @@
+export const projects = [
+  {
+    slug: 'barcelona-balmes',
+    title: 'Barcelona, Balmes',
+    description: 'Reforma integral de un piso en Barcelona',
+    alt: 'Barcelona Balmes',
+    cover: 'barcelona-balmes-thumbail.jpeg',
+    images: [
+      {
+        src: 'barcelona-balmes-01.jpeg',
+        alt: 'Barcelona Balmes'
+      },
+      {
+        src: 'barcelona-balmes-02.jpeg',
+        alt: 'Barcelona Balmes'
+      },
+      {
+        src: 'barcelona-balmes-03.jpeg',
+        alt: 'Barcelona Balmes'
+      },
+      {
+        src: 'barcelona-balmes-04.jpeg',
+        alt: 'Barcelona Balmes'
+      },
+      {
+        src: 'barcelona-balmes-05.jpeg',
+        alt: 'Barcelona Balmes'
+      },
+      {
+        src: 'barcelona-balmes-06.jpeg',
+        alt: 'Barcelona Balmes'
+      },
+      {
+        src: 'barcelona-balmes-07.jpeg',
+        alt: 'Barcelona Balmes'
+      },
+      {
+        src: 'barcelona-balmes-08.jpeg',
+        alt: 'Barcelona Balmes'
+      },
+      {
+        src: 'barcelona-balmes-09.jpeg',
+        alt: 'Barcelona Balmes'
+      },
+      {
+        src: 'barcelona-balmes-10.jpeg',
+        alt: 'Barcelona Balmes'
+      },
+    ],
+  },
+  {
+    slug: 'barcelona-example',
+    title: 'Barcelona, Example',
+    description: 'Reforma integral de un piso en Barcelona',
+    alt: 'Barcelona Example',
+    cover: 'barcelona-example-thumbail.jpeg',
+    images: [
+      {
+        src: 'barcelona-example-01.jpeg',
+        alt: 'Barcelona Example'
+      },
+      {
+        src: 'barcelona-example-02.jpeg',
+        alt: 'Barcelona Example'
+      },
+      {
+        src: 'barcelona-example-03.jpeg',
+        alt: 'Barcelona Example'
+      },
+      {
+        src: 'barcelona-example-04.jpeg',
+        alt: 'Barcelona Example'
+      },
+      {
+        src: 'barcelona-example-05.jpeg',
+        alt: 'Barcelona Example'
+      },
+      {
+        src: 'barcelona-example-06.jpeg',
+        alt: 'Barcelona Example'
+      },
+      {
+        src: 'barcelona-example-07.jpeg',
+        alt: 'Barcelona Example'
+      },
+      {
+        src: 'barcelona-example-08.jpeg',
+        alt: 'Barcelona Example'
+      },
+      {
+        src: 'barcelona-example-09.jpeg',
+        alt: 'Barcelona Example'
+      },
+      {
+        src: 'barcelona-example-10.jpeg',
+        alt: 'Barcelona Example'
+      },
+    ],
+  }
+]
